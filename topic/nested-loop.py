b187fe03835288299d60f5547df00a686e7e0113
kamrul-CSE-official <kamrulhasan635653@gmail.com>
2024-03-15 20:11:00 +0600

    update

---
+++ topic/nested-loop.py
@@ -0,0 +1,30 @@
+# n = input()
+n = 5
+n = int(n)
+
+count = 0
+
+for i in range(n):
+    for j in range(n):
+        count += 1
+print("n =", n, " count: ", count)
+
+# n = input()
+n = 5
+n = int(n)
+
+count = 0
+
+for i in range(n):
+    count += 1
+print("n =", n, " count: ", count)
+
+n = input()
+n = int(n)
+count = 0
+
+for i in range(n):
+    for j in range(n):
+        for k in range(n):
+            count += 1
+print("n =", n, " count: ", count)
