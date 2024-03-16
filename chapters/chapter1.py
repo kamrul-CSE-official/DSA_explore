7f178c31f0cf5b4f176b5736261e0ef132ff763c
kamrul-CSE-official <kamrulhasan635653@gmail.com>
2024-03-16 19:50:28 +0600

    update

--- chapters/chapter1.py
+++ chapters/chapter1.py
@@ -1,4 +1,23 @@
 
+n = input()
+n = int(n)
+count = 0
+
+for i in range(n):
+    count += 1
+for i in range(n):
+    for j in range(n):
+        count += 1
+print("Count value", count)
+
+def average(l):
+    if not l:
+        return None
+    return sum(l) / len(l)
+if __name__ == "__main__":
+    l = [1, 2, 3, 4, 5]
+    print(average(l))
+
 def firsttype():
     n = input()
     n = int(n)
@@ -12,3 +31,4 @@ def secondtype():
     for i in range(1, n + 1):
         result = result + i
     print(result)
+
