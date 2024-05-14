ad771575e3665442a6508b0002a9abc3189fc788
kamrul-CSE-official <kamrulhasan635653@gmail.com>
2024-05-15 00:04:00 +0600

    update

--- tutorial/list.py
+++ tutorial/list.py
@@ -1,6 +1,9 @@
 grocery_list = ["Rice", "Potato", "Tomato"]
 l2 = list()
 
+second_item = grocery_list[1]
+print(second_item)
+
 
 print(grocery_list)
 
@@ -11,3 +14,21 @@ print(grocery_list)
 l2.append(4)
 l2.append("Computer")
 print(l2)
+
+# 2
+
+print(grocery_list[-1])
+
+print(grocery_list)
+grocery_list.sort()
+print(grocery_list)
+
+numbers = [1, 23, 2, 3, 45, 31, 4]
+print(numbers)
+numbers.sort()
+print(numbers)
+
+sorted_list = sorted(numbers)
+print(sorted_list)
+
+
