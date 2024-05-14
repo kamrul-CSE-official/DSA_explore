20b0ce4cefdc05c7d2b125f78a076b70021d137a
kamrul-CSE-official <kamrulhasan635653@gmail.com>
2024-05-14 23:54:46 +0600

    list

---
+++ tutorial/list.py
@@ -0,0 +1,13 @@
+grocery_list = ["Rice", "Potato", "Tomato"]
+l2 = list()
+
+
+print(grocery_list)
+
+grocery_list.append("Water")
+
+print(grocery_list)
+
+l2.append(4)
+l2.append("Computer")
+print(l2)
