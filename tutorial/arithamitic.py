562719d592fc89f55b889dabbedeb645f80cde16
kamrul-CSE-official <kamrulhasan635653@gmail.com>
2024-05-14 23:50:00 +0600

    Arithamitic recap

---
+++ tutorial/arithamitic.py
@@ -0,0 +1,27 @@
+number_value = 3
+second_value = 4
+
+addition = number_value + second_value
+subTraction = number_value - second_value
+mult = number_value * 3
+divi = number_value / 5
+divi_int = number_value // 5
+remainder = number_value % 5
+
+print(f"Addition: {addition}")
+print(f"Sub: {subTraction}")
+print(f"Multipication: {mult}")
+print(f"Divition: {divi}")
+print(f"Divition int: {divi_int}")
+print(f"Remainder: {remainder}")
+
+person_name = "MD.Kamrul Hasan"
+food = "Rice"
+value2 = 9
+
+kamrul_with_value = person_name + " "+ str(value2)
+concatenated_string = kamrul_with_value + " " + food
+print(concatenated_string)
+
+
+print("Length of name: ", len(person_name))
